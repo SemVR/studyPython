f3b67c4a0e770087676fb0adc4f53a708f09bd4c
SemVR <sem.wlad26@gmail.com>
2026-02-10 16:51:40 +0200

    task 11.1 11.2 11.3 completed

---
+++ homework_11.2.py
@@ -0,0 +1,19 @@
+#Filling a list with cubes of numbers
+
+def generate_cube_numbers(limit):
+    num = 2
+    while True:
+        cube = num ** 3
+        if cube > limit:
+            return
+        yield cube
+        num += 1
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+print('OK')
